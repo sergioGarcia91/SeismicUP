e0b0eb2b526302ebdd2705f73deebf5e977000d4
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2025-12-26 08:46:25 -0500

    Create __init__.py

---
+++ seismicup/__init__.py
@@ -0,0 +1,14 @@
+"""
+SeismicUP
+Author: Sergio García-Arias
+""
+
+# Metadata del paquete
+__author__ = "Sergio García"
+__version__ = "0.1.0"
+
+# Subpaquetes principales
+#from . import io
+from . import plots
+from . import stats
+from . import utils
