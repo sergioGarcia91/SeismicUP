a12d2a51f58ea9852cdd219b2ea5484ad1b16370
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2025-12-26 09:46:35 -0500

    Update __init__.py

--- seismicup/plots/__init__.py
+++ seismicup/plots/__init__.py
@@ -1,1 +1,5 @@
+from .corte_plot import plot_corte
+from .hist_plot import histograma_plot
+from .save_plot import save_plot
 from .style_plot import get_TimesNewRoman_font
+from .time_mag_plot import time_magnitud_plot
