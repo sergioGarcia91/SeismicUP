4aaf99a11387d941cb4e02f1baa88182611464d0
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2025-12-26 09:44:38 -0500

    Create corte_plot.py

---
+++ seismicup/plots/corte_plot.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+ 
+def plot_corte(df,
+               columna_x,
+               columna_y,
+               filtro_corte,
+               ax_s,
+               label_s,
+               invert_yaxis=True):
+  x_min = df[columna_x].min()
+  x_max = df[columna_x].max()
+  y_min = df[columna_y].min()
+  y_max = df[columna_y].max()
+
+  df = df[filtro_datos].copy()
+  df.reset_index(drop=True, inplace=True)
+
+  ax_s.scatter(df[columna_x],
+               df[columna_y],
+               s=1,
+               label=label_s,
+               alpha=0.8)
+  ax_s.set_xlim(x_min, x_max)
+
+  ax_s.set_xlim(x_min, x_max)
+  ax_s.set_ylim(y_min, y_max)
+  ax_s.grid(ls='--', c='gray', alpha=0.7)
+  ax_s.set_xlabel(columna_x)
+  ax_s.set_ylabel(columna_y)
+
+  if invert_yaxis:
+    ax_s.invert_yaxis()
