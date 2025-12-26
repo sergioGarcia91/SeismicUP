ea699c0eb23eb96be5e73b52fab994741c3ed32d
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2025-12-26 11:26:25 -0500

    Update __init__.py

--- seismicup/stats/__init__.py
+++ seismicup/stats/__init__.py
@@ -1,1 +1,5 @@
-
+from .get_N_LGR import obtener_N_LGR
+from .mc_estabilidad_b import mc_estabilidad_b
+from .mc_max_N_log10 import mc_maximo_N_log10
+from .mc_max_curv import mc_maxima_curvatura
+from .mc_menor_pend import mc_menor_pendiente
