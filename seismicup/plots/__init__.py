588e49977d43f8c29819afbd01955db502921621
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2025-12-30 13:48:48 -0500

    Update __init__.py

--- seismicup/plots/__init__.py
+++ seismicup/plots/__init__.py
@@ -5,4 +5,4 @@ from .style_plot import get_TimesNewRoman_font
 from .time_mag_plot import time_magnitud_plot
 from .grl_scatter import GutenbergRichter_scatter_plot
 from .grl_line_plot import GutenbergRichter_line_plot
-from .hist_grl_noise.py import histograma_GRL_noise_plot
+from .hist_grl_noise import histograma_GRL_noise_plot
