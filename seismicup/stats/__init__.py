e99e4e74758fe90cca95b41803f9b87ef6ea3e09
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2025-12-30 10:01:53 -0500

    Update __init__.py

--- seismicup/stats/__init__.py
+++ seismicup/stats/__init__.py
@@ -7,3 +7,5 @@ from .get_grl_values_noise import get_GutenbergRichter_values_noised
 from .get_grl_values import get_GutenbergRichter_values
 from .get_grl_values_boost import get_GutenbergRichter_values_boosted
 from .retornos_ import periodo_retorno
+from .corr_integral import correlacion_integral_xy
+from .estimar_dc import estimar_Dc
