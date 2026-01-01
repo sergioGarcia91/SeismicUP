27dfaa46eacecb47ffc7343c2b2f3352478f91cb
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2026-01-01 07:49:07 -0500

    Update hist_plot.py

--- seismicup/plots/hist_plot.py
+++ seismicup/plots/hist_plot.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
  
-def histograma_plot(df, columna, ax_h, step_, dist_min, y_log = True):
+def histograma_plot(df, 
+                    columna, 
+                    ax_h, 
+                    step_, 
+                    dist_min=0, 
+                    dist_max=None,
+                    units_label='km',
+                    y_log = True):
   p10 = np.round(df[columna].quantile(q=0.1), 2)
   p25 = np.round(df[columna].quantile(q=0.25), 2) # q1
   p50 = np.round(df[columna].quantile(q=0.5), 2) # q2
@@ -12,7 +19,8 @@ def histograma_plot(df, columna, ax_h, step_, dist_min, y_log = True):
 
   step_ = step_
   dist_min = dist_min # valor minimo del histograma
-  dist_max = step_ * (1 + int(df[columna].max() // step_) )
+  if dist_max == None:
+    dist_max = step_ * (1 + int(df[columna].max() // step_) )
   bins_h = np.arange(start=dist_min, stop=dist_max+step_, step=step_)
   xlim_h = [dist_min-step_, bins_h.max()+step_]
 
@@ -23,11 +31,11 @@ def histograma_plot(df, columna, ax_h, step_, dist_min, y_log = True):
               ax= ax_h,
               legend= False)
   # quantiles
-  ax_h.axvline(x=p10, label=f'P10: {p10} km', c='k', ls='--')
-  ax_h.axvline(x=p25, label=f'Q1: {p25} km', c='b', ls='--')
-  ax_h.axvline(x=p50, label=f'Q2: {p50} km', c='r', ls='--')
-  ax_h.axvline(x=p75, label=f'Q3: {p75} km', c='b', ls='--')
-  ax_h.axvline(x=p90, label=f'P90: {p90} km', c='k', ls='--')
+  ax_h.axvline(x=p10, label=f'P10: {p10} {units_label}', c='k', ls='--')
+  ax_h.axvline(x=p25, label=f'Q1: {p25} {units_label}', c='b', ls='--')
+  ax_h.axvline(x=p50, label=f'Q2: {p50} {units_label}', c='r', ls='--')
+  ax_h.axvline(x=p75, label=f'Q3: {p75} {units_label}', c='b', ls='--')
+  ax_h.axvline(x=p90, label=f'P90: {p90} {units_label}', c='k', ls='--')
 
   ax_h.set_xlim(left=xlim_h[0], right=xlim_h[1])
   ax_h.grid(ls='--', c='gray', alpha=0.7)
