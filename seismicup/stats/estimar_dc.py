b0fa91b3967a78a027228a0ea215171e5270f0d7
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2026-01-03 06:31:59 -0500

    Update estimar_dc.py

--- seismicup/stats/estimar_dc.py
+++ seismicup/stats/estimar_dc.py
@@ -14,4 +14,4 @@ def estimar_Dc(r_km, C, idx_range=None):
         sel = np.zeros_like(xr, bool)
         sel[idx_range[0]:idx_range[1]] = True
     slope, intercept, *_ = stats.linregress(xr[sel], yr[sel])
-    return slope, intercept, (xr, yr, sel)   # slope = Dc
+    return slope, intercept, (xr, yr, sel, m)   # slope = Dc
