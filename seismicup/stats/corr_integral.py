c478271663aab8f153e81a6e3479821ecb2eeb3e
Sergio Andrés García Arias <108496196+sergioGarcia91@users.noreply.github.com>
2026-06-19 13:52:39 -0500

    Update corr_integral.py

--- seismicup/stats/corr_integral.py
+++ seismicup/stats/corr_integral.py
@@ -14,3 +14,19 @@ def correlacion_integral_xy(x_km, y_km, radio_en_km):
         total_dir = sum(len(lst) - 1 for lst in neigh)   # pares dirigidos (i→j)
         C.append(total_dir / (N*(N-1)))                  # equivale a 2*N_pairs/[N(N-1)]
     return np.asarray(C)
+
+def correlacion_integral_xyz(x_km, y_km, z_km, radio_en_km):
+    P = np.c_[x_km, y_km, z_km].astype(float)
+    N = len(P)
+
+    if N < 2:
+        raise ValueError("Se necesitan al menos 2 puntos")
+
+    tree = cKDTree(P)
+    C = []
+
+    for r in radio_en_km:
+        total_dir = tree.count_neighbors(tree, r) - N
+        C.append(total_dir / (N * (N - 1)))
+
+    return np.asarray(C)
